b3c7f6e24a80fe4c46d8b82cffcbefca0d1b87a8
Kris Pardo <kpardo@caltech.edu>
2022-03-14 09:16:58 -0700

    multiple mphi supported. added self-interactions to plots.

--- drivers/get_limits.py
+++ drivers/get_limits.py
@@ -4,7 +4,7 @@ import astropy.constants as const
 from dataclasses import dataclass
 from scipy.integrate import quad_vec
 from scipy.special import spherical_jn
-from astropy.table import Table
+from astropy.table import Table, hstack
 import datetime
 
 from aidm.const import tp, lp, rhox, vesc, vdm
@@ -13,12 +13,12 @@ from aidm.cross_sections import cs_limit
 
 exps = ['GDM', 'MAQRO', 'Pino', 'BECCAL']
 mxs = np.logspace(-10, 4, 100)*u.MeV
-mphiratios = 1.e-5
+mphiratios = [1.e-5, 1.e-3, 1.e-2]
+get_noqmin = True
 
 
 def get_lim(ex, qmin=True, mphi_ratio = mphiratios, medtype='light'):
-    print(datetime.datetime.now())
-    print(f'Getting limits for {ex} with qmin={qmin}')
+    print(f'{datetime.datetime.now()}: Getting limits for {ex} with qmin={qmin}')
     ## Define exp. w/ qmin
     class_ = getattr(x, ex)
     exp = class_()
@@ -32,16 +32,30 @@ def get_lim(ex, qmin=True, mphi_ratio = mphiratios, medtype='light'):
         lims = cs_limit(mxs, ex=exp, medtype=medtype, mphi=mphi_ratio*mxs)
 
     ## Save to file.
-    if medtype=='light':
-        fn = f'../results/limits/{ex}_{qmin}_{medtype[0]}_{int(-1.*np.log10(mphi_ratio))}.dat'
-    else:
+    try:
+        len(mphi_ratio)
         fn = f'../results/limits/{ex}_{qmin}_{medtype[0]}.dat'
-    t = Table([mxs, lims],
-        names=('mx', 'sigma'))
-    t.write(fn, format='ascii.csv', overwrite=True)
-    print(f'Saved to file {fn}')
+    except TypeError:
+        fn = f'../results/limits/{ex}_{qmin}_{medtype[0]}_{int(-1.*np.log10(mphi_ratio))}.dat'
+
+    tm = Table()
+    tm['mx'] = mxs
+    limT = Table([l for l in lims])
+    t = hstack([tm, limT])
+    try:
+        names = [f'sigma_{int(-1.*np.log10(mpr))}' for mpr in mphi_ratio]
+        old = [f'col{i}' for i in range(len(mphi_ratio))]
+    except TypeError:
+        names = [f'sigma_{int(-1.*np.log10(mphi_ratio))}']
+        old = ['col0']
+    t.rename_columns(names=old, new_names=names)
+    t.write(fn, format='ascii.ecsv', overwrite=True)
+    print(f'Best cross section is {np.min(lims)} at mx = {mxs[np.argmin(lims)]}.')
+    print(f'{datetime.datetime.now()}: Saved to file {fn}')
     return 0
 
 ## Run
 for ex in exps:
     get_lim(ex)
+    if get_noqmin:
+        get_lim(ex, qmin=False)
