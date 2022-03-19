3141af77e7b5ef8098ce85d2f7501d353eabce9c
Kris Pardo <kpardo@caltech.edu>
2022-03-18 17:40:13 -0700

    added code to make heavy mediator run easier. ran for more light mediator ratios.

--- drivers/get_limits.py
+++ drivers/get_limits.py
@@ -13,9 +13,9 @@ from aidm.cross_sections import cs_limit
 
 exps = ['GDM', 'MAQRO', 'Pino', 'BECCAL']
 mxs = np.logspace(-10, 4, 100)*u.MeV
-mphiratios = [1.e-5, 1.e-3, 1.e-2]
+mphiratios = [1.e-10, 1.e-7, 1.e-5, 1.e-4, 1.e-3, 1.e-2]
 get_noqmin = True
-
+med = 'light'
 
 def get_lim(ex, qmin=True, mphi_ratio = mphiratios, medtype='light'):
     print(f'{datetime.datetime.now()}: Getting limits for {ex} with qmin={qmin}')
@@ -32,22 +32,32 @@ def get_lim(ex, qmin=True, mphi_ratio = mphiratios, medtype='light'):
         lims = cs_limit(mxs, ex=exp, medtype=medtype, mphi=mphi_ratio*mxs)
 
     ## Save to file.
-    try:
-        len(mphi_ratio)
+    if medtype == 'heavy':
         fn = f'../results/limits/{ex}_{qmin}_{medtype[0]}.dat'
-    except TypeError:
-        fn = f'../results/limits/{ex}_{qmin}_{medtype[0]}_{int(-1.*np.log10(mphi_ratio))}.dat'
+    else:
+        try:
+            len(mphi_ratio)
+            fn = f'../results/limits/{ex}_{qmin}_{medtype[0]}.dat'
+        except TypeError:
+            fn = f'../results/limits/{ex}_{qmin}_{medtype[0]}_{int(-1.*np.log10(mphi_ratio))}.dat'
 
     tm = Table()
     tm['mx'] = mxs
-    limT = Table([l for l in lims])
-    t = hstack([tm, limT])
     try:
-        names = [f'sigma_{int(-1.*np.log10(mpr))}' for mpr in mphi_ratio]
-        old = [f'col{i}' for i in range(len(mphi_ratio))]
+        limT = Table([l for l in lims])
     except TypeError:
-        names = [f'sigma_{int(-1.*np.log10(mphi_ratio))}']
-        old = ['col0']
+        limT = Table([lims])
+    t = hstack([tm, limT])
+    if medtype == 'heavy':
+        names = [f'sigma']
+        old = [f'col0']
+    else:
+        try:
+            names = [f'sigma_{int(-1.*np.log10(mpr))}' for mpr in mphi_ratio]
+            old = [f'col{i}' for i in range(len(mphi_ratio))]
+        except TypeError:
+            names = [f'sigma_{int(-1.*np.log10(mphi_ratio))}']
+            old = ['col0']
     t.rename_columns(names=old, new_names=names)
     t.write(fn, format='ascii.ecsv', overwrite=True)
     print(f'Best cross section is {np.min(lims)} at mx = {mxs[np.argmin(lims)]}.')
@@ -56,6 +66,6 @@ def get_lim(ex, qmin=True, mphi_ratio = mphiratios, medtype='light'):
 
 ## Run
 for ex in exps:
-    get_lim(ex)
+    get_lim(ex, medtype=med)
     if get_noqmin:
-        get_lim(ex, qmin=False)
+        get_lim(ex, qmin=False, medtype=med)
