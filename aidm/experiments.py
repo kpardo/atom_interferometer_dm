6a58b44b97780756195f02348bf6d072a0fe7e68
Kris Pardo <kpardo@caltech.edu>
2024-05-28 16:47:29 -0700

    add AEDGE. Take out decoherence curves for diffuse clouds.

--- aidm/experiments.py
+++ aidm/experiments.py
@@ -17,89 +17,111 @@ class Experiment:
     deltax: float
     Nmeas: float
     Texp: float
-    qmin: float = 1.e-30*u.MeV
+    qmin: float = 1.0e-30 * u.MeV
 
     def __post_init__(self):
-        self.mT = (const.m_p*self.N).to(u.MeV)
+        self.mT = (const.m_p * self.N).to(u.MeV)
         self.phimin = self.get_phimin()
 
     def get_phimin(self):
-    #    return (0.25*const.m_p.to(u.MeV)*self.A*self.deltax*self.amin*self.Texp).to('')
-        return 1./np.sqrt(self.N/self.A) ## sqrt(N_atoms)
+        #    return (0.25*const.m_p.to(u.MeV)*self.A*self.deltax*self.amin*self.Texp).to('')
+        return 1.0 / np.sqrt(self.N / self.A)  ## sqrt(N_atoms)
+
 
 @dataclass
 class GDM(Experiment):
     name: str = "GDM"
     N: float = 8.7e9
     A: float = 87
-    amin: float = (2.2e-15*u.m/u.s**2*1./const.c).to(u.Hz)
-    r: float = (1.e-3*u.m*lp).to(u.MeV**(-1))
-    deltax: float = (25*u.m*lp).to(u.MeV**(-1))
-    Texp: float = 20.*u.s
-    Nmeas: int = int((1.*u.yr/Texp).to(''))
-    etadm: float = 1.
-    etabkg: float = 1.
-    Nind: float = 1.e8
+    amin: float = (2.2e-15 * u.m / u.s**2 * 1.0 / const.c).to(u.Hz)
+    r: float = (1.0e-3 * u.m * lp).to(u.MeV ** (-1))
+    deltax: float = (25 * u.m * lp).to(u.MeV ** (-1))
+    Texp: float = 20.0 * u.s
+    Nmeas: int = int((1.0 * u.yr / Texp).to(""))
+    etadm: float = 1.0
+    etabkg: float = 1.0
+    Nind: float = 1.0e8
+
 
 @dataclass
 class BECCAL(Experiment):
-    name: str="BECCAL"
+    name: str = "BECCAL"
     N: float = 8.7e7
     A: float = 87
-    amin: float = (3.e-12*u.m/u.s**2*1./const.c).to(u.Hz)
-    r: float = (1.5e-4*u.m*lp).to(u.MeV**(-1))
-    deltax: float = (3.e-3*u.m*lp).to(u.MeV**(-1))
-    Texp: float = 2.6*u.s
-    Nmeas: int = int((1.*u.yr/Texp).to(''))
+    amin: float = (3.0e-12 * u.m / u.s**2 * 1.0 / const.c).to(u.Hz)
+    r: float = (1.5e-4 * u.m * lp).to(u.MeV ** (-1))
+    deltax: float = (3.0e-3 * u.m * lp).to(u.MeV ** (-1))
+    Texp: float = 2.6 * u.s
+    Nmeas: int = int((1.0 * u.yr / Texp).to(""))
     etadm: float = 0.5
     etabkg: float = 0.001
-    Nind: float = 1.e6
+    Nind: float = 1.0e6
+
 
 @dataclass
 class MAQRO(Experiment):
-    name: str='MAQRO'
-    N: float = 1.e10
+    name: str = "MAQRO"
+    N: float = 1.0e10
     A: float = 60
-    amin: float = (1.e-13*u.m/u.s**2*1./const.c).to(u.Hz)
-    r: float = (120.e-9*u.m*lp).to(u.MeV**(-1))
-    deltax: float = (100.e-9*u.m*lp).to(u.MeV**(-1))
-    Texp: float = 100.*u.s
-    Nmeas: int = int((1.*u.yr/Texp).to(''))
+    amin: float = (1.0e-13 * u.m / u.s**2 * 1.0 / const.c).to(u.Hz)
+    r: float = (120.0e-9 * u.m * lp).to(u.MeV ** (-1))
+    deltax: float = (100.0e-9 * u.m * lp).to(u.MeV ** (-1))
+    Texp: float = 100.0 * u.s
+    Nmeas: int = int((1.0 * u.yr / Texp).to(""))
     etadm: float = 1.0
     etabkg: float = 1.0
     Nind: float = 1.0
 
     def __post_init__(self):
-        self.mT = (const.m_p*self.N).to(u.MeV)
-        self.phimin = 1.
+        self.mT = (const.m_p * self.N).to(u.MeV)
+        self.phimin = 1.0
+
 
 @dataclass
 class Pino(Experiment):
     name: str = "Pino"
     N: float = 2.2e13
     A: float = 93
-    amin: float = (2.5e-12*u.m/u.s**2*1./const.c).to(u.Hz)
-    r: float = (1.e-6*u.m*lp).to(u.MeV**(-1))
-    deltax: float = (290.e-9*u.m*lp).to(u.MeV**(-1))
-    Texp: float = 0.483*u.s
-    Nmeas: int = int((1.*u.yr/Texp).to(''))
+    amin: float = (2.5e-12 * u.m / u.s**2 * 1.0 / const.c).to(u.Hz)
+    r: float = (1.0e-6 * u.m * lp).to(u.MeV ** (-1))
+    deltax: float = (290.0e-9 * u.m * lp).to(u.MeV ** (-1))
+    Texp: float = 0.483 * u.s
+    Nmeas: int = int((1.0 * u.yr / Texp).to(""))
     etadm: float = 0.5
     etabkg: float = 0.001
     Nind: float = 1.0
 
     def __post_init__(self):
-        self.mT = (const.m_p*self.N).to(u.MeV)
-        self.phimin = 1.
+        self.mT = (const.m_p * self.N).to(u.MeV)
+        self.phimin = 1.0
+
 
 @dataclass
 class Stanford(Experiment):
     ## drop tower described in Asenbaum + (2020) PRL
-    name: str = 'Stanford'
-    N: float = 87*4.e6
+    name: str = "Stanford"
+    N: float = 87 * 4.0e6
     A: float = 87
-    amin: float = (1.372e-10*u.m/u.s**2*1./const.c).to(u.Hz)
-    r: float = (200.e-6*u.m*lp).to(u.MeV**(-1))
-    deltax: float = (0.067*u.m*lp).to(u.MeV**(-1))
-    Texp: float = 1.910*u.s
-    Nmeas: int = int((1.*u.yr/Texp).to(''))
-    Nind: int = 4.e6
+    amin: float = (1.372e-10 * u.m / u.s**2 * 1.0 / const.c).to(u.Hz)
+    r: float = (200.0e-6 * u.m * lp).to(u.MeV ** (-1))
+    deltax: float = (0.067 * u.m * lp).to(u.MeV ** (-1))
+    Texp: float = 1.910 * u.s
+    Nmeas: int = int((1.0 * u.yr / Texp).to(""))
+    Nind: int = 4.0e6
+
+
+@dataclass
+class AEDGE(Experiment):
+    name: str = "AEDGE"
+    N: float = 87 * 1.0e10
+    A: float = 87
+    amin: float = 0  ## Not used.
+    phimin: float = 1.0e-5
+    r: float = (3.0e-3 * u.m * lp).to(u.MeV ** (-1))
+    deltax: float = (0.9 * u.m * lp).to(u.MeV ** (-1))
+    Texp: float = 600 * u.s
+    Nmeas: int = int((1.0 * u.yr / Texp).to(""))
+    Nind: int = 1.0e10
+
+    def __post_init__(self):
+        self.mT = (const.m_p * self.N).to(u.MeV)
--- drivers/get_limits.py
+++ drivers/get_limits.py
@@ -1,10 +1,10 @@
-'''
+"""
 this script gets the cross section limits for given inputs.
 inputs: mediator type, phase
 
 example:
 python get_limits.py 'light' False
-'''
+"""
 
 import numpy as np
 import astropy.units as u
@@ -21,8 +21,9 @@ import aidm.experiments as x
 from aidm.cross_sections import cs_limit, cs_limit_mod
 
 # exps = ['GDM', 'MAQRO', 'Pino', 'BECCAL', 'Stanford']
-exps = ['MAQRO', 'Pino', 'BECCAL', 'Stanford', 'GDM']
-mxs = np.logspace(-6.5, 3.5, 1000)*u.MeV
+# exps = ['MAQRO', 'Pino', 'BECCAL', 'Stanford', 'GDM']
+exps = ["AEDGE"]
+mxs = np.logspace(-6.5, 3.5, 1000) * u.MeV
 # mxs = np.logspace(-5,3,1000)*u.MeV
 
 try:
@@ -31,62 +32,82 @@ try:
     assert isinstance(phase, bool)
 except:
     print("Oops you didn't give a valid input! Running default.")
-    med = 'light'
+    med = "light"
     phase = False
 
-if med == 'light':
-    mphiratios = [1.e-10, 1.e-9, 1.e-7, 1.e-6, 1.e-5, 1.e-4, 1.e-3, 1.e-2]
+if med == "light":
+    mphiratios = [1.0e-10, 1.0e-9, 1.0e-7, 1.0e-6, 1.0e-5, 1.0e-4, 1.0e-3, 1.0e-2]
     # mphiratios = [1.e-4, 1.e-3]
-elif med == 'fixed_light':
-    mphiratios = (1.*u.eV).to(u.MeV).value
+elif med == "fixed_light":
+    mphiratios = (1.0 * u.eV).to(u.MeV).value
 else:
-    mphiratios = 1.
+    mphiratios = 1.0
 
-def get_lim(ex, mphi_ratio = mphiratios, medtype='light',
-    phase = phase):
+
+def get_lim(ex, mphi_ratio=mphiratios, medtype="light", phase=phase):
     if phase:
-        print(f'{datetime.datetime.now()}: Getting {medtype} mediator phase limits for {ex}')
+        print(
+            f"{datetime.datetime.now()}: Getting {medtype} mediator phase limits for {ex}"
+        )
     else:
-        print(f'{datetime.datetime.now()}: Getting {medtype} mediator decoherence limits for {ex}')
+        print(
+            f"{datetime.datetime.now()}: Getting {medtype} mediator decoherence limits for {ex}"
+        )
     ## Define exp. w/ qmin
     class_ = getattr(x, ex)
     exp = class_()
 
     ## Get limits
-    if (phase) and (med == 'light'):
-        lims = [[cs_limit(mx, ex=exp, medtype=medtype, mphi=mpr*mx, phase=phase) for mx in mxs] for mpr in mphi_ratio]
-    elif (phase) and (med == 'heavy'):
-        if ex in ['BECCAL', 'GDM', 'Stanford']:
+    if (phase) and (med == "light"):
+        lims = [
+            [
+                cs_limit(mx, ex=exp, medtype=medtype, mphi=mpr * mx, phase=phase)
+                for mx in mxs
+            ]
+            for mpr in mphi_ratio
+        ]
+    elif (phase) and (med == "heavy"):
+        if ex in ["BECCAL", "GDM", "Stanford"]:
             ## for numerical issues
-            mxs_ = np.logspace(-5, 3, 5000)*u.MeV
+            mxs_ = np.logspace(-5, 3, 5000) * u.MeV
         else:
-            mxs_ = np.logspace(-6.5, 3.5, 1000)*u.MeV
+            mxs_ = np.logspace(-6.5, 3.5, 1000) * u.MeV
         lims = [cs_limit(mx, ex=exp, medtype=medtype, phase=phase) for mx in mxs_]
-    elif (~phase) and (med == 'heavy'):
-        if ex in ['BECCAL', 'GDM', 'Stanford']:
+    elif (~phase) and (med == "heavy"):
+        if ex in ["BECCAL", "GDM", "Stanford"]:
             ## for numerical issues
-            mxs_ = np.logspace(-5, 3, 5000)*u.MeV
+            mxs_ = np.logspace(-5, 3, 5000) * u.MeV
         else:
-            mxs_ = np.logspace(-6.5, 3.5, 1000)*u.MeV
+            mxs_ = np.logspace(-6.5, 3.5, 1000) * u.MeV
         lims = cs_limit(mxs_, ex=exp, medtype=medtype, phase=phase)
-    elif (phase) and (med == 'fixed_light'):
-        lims = [cs_limit(mx, ex=exp, medtype='light', mphi=mphiratios*u.MeV, phase=phase) for mx in mxs]
-    elif (~phase) and (med == 'fixed_light'):
-        lims = cs_limit(mxs, ex=exp, medtype='light', mphi=mphiratios*u.MeV, phase=phase)
+    elif (phase) and (med == "fixed_light"):
+        lims = [
+            cs_limit(mx, ex=exp, medtype="light", mphi=mphiratios * u.MeV, phase=phase)
+            for mx in mxs
+        ]
+    elif (~phase) and (med == "fixed_light"):
+        lims = cs_limit(
+            mxs, ex=exp, medtype="light", mphi=mphiratios * u.MeV, phase=phase
+        )
     else:
         try:
-            lims = [cs_limit(mxs, ex=exp, medtype=medtype, mphi=mpr*mxs, phase=phase) for mpr in mphi_ratio]
+            lims = [
+                cs_limit(mxs, ex=exp, medtype=medtype, mphi=mpr * mxs, phase=phase)
+                for mpr in mphi_ratio
+            ]
         except TypeError:
-            lims = cs_limit(mxs, ex=exp, medtype=medtype, mphi=mphi_ratio*mxs, phase=phase)
+            lims = cs_limit(
+                mxs, ex=exp, medtype=medtype, mphi=mphi_ratio * mxs, phase=phase
+            )
     ## Save to file.
-    if medtype == 'heavy':
-        fn = f'../results/limits/{ex}_{medtype[0]}'
+    if medtype == "heavy":
+        fn = f"../results/limits/{ex}_{medtype[0]}"
     else:
         try:
             len(mphi_ratio)
-            fn = f'../results/limits/{ex}_{medtype[0]}'
+            fn = f"../results/limits/{ex}_{medtype[0]}"
         except TypeError:
-            fn = f'../results/limits/{ex}_{medtype[0]}_{int(-1.*np.log10(mphi_ratio))}'
+            fn = f"../results/limits/{ex}_{medtype[0]}_{int(-1.*np.log10(mphi_ratio))}"
 
     if phase == True:
         fn += "_phase.dat"
@@ -94,32 +115,33 @@ def get_lim(ex, mphi_ratio = mphiratios, medtype='light',
         fn += ".dat"
 
     tm = Table()
-    tm['mx'] = mxs
-    if (med == 'heavy') and (ex in ['BECCAL', 'GDM', 'Stanford']):
-        tm['mx'] = np.logspace(-5, 3, 5000)*u.MeV
+    tm["mx"] = mxs
+    if (med == "heavy") and (ex in ["BECCAL", "GDM", "Stanford"]):
+        tm["mx"] = np.logspace(-5, 3, 5000) * u.MeV
     try:
         limT = Table([l for l in lims])
     except TypeError:
         limT = Table([lims])
     t = hstack([tm, limT])
-    if medtype == 'heavy':
-        names = [f'sigma']
-        old = [f'col0']
+    if medtype == "heavy":
+        names = [f"sigma"]
+        old = [f"col0"]
     else:
         try:
-            names = [f'sigma_{int(-1.*np.log10(mpr))}' for mpr in mphi_ratio]
-            old = [f'col{i}' for i in range(len(mphi_ratio))]
+            names = [f"sigma_{int(-1.*np.log10(mpr))}" for mpr in mphi_ratio]
+            old = [f"col{i}" for i in range(len(mphi_ratio))]
         except TypeError:
-            names = [f'sigma_{int(-1.*np.log10(mphi_ratio))}']
-            old = ['col0']
+            names = [f"sigma_{int(-1.*np.log10(mphi_ratio))}"]
+            old = ["col0"]
     t.rename_columns(names=old, new_names=names)
-    t.write(fn, format='ascii.ecsv', overwrite=True)
-    print(f'{datetime.datetime.now()}: Saved to file {fn}')
+    t.write(fn, format="ascii.ecsv", overwrite=True)
+    print(f"{datetime.datetime.now()}: Saved to file {fn}")
     try:
-        print(f'Best cross section is {np.min(lims)} at mx = {mxs[np.argmin(lims)]}.')
+        print(f"Best cross section is {np.min(lims)} at mx = {mxs[np.argmin(lims)]}.")
     except:
         return 0
     return 0
+
 
 ## Run
 for ex in exps:
